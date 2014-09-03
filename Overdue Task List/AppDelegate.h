a69a0ca064c7f4dcd492b2e1bd2d42a4b60962fc
David Pirih <david@OSXMBAPIRI01.local>
2014-09-03 08:16:01 +0200

    Initial Commit

---
+++ Overdue Task List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Overdue Task List
+//
+//  Created by David Pirih on 03.09.14.
+//  Copyright (c) 2014 piri-piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Overdue Task List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Overdue Task List
+//
+//  Created by David Pirih on 03.09.14.
+//  Copyright (c) 2014 piri-piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
